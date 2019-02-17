2c94af9596eac4c51d45a536f5ef1918563648cf
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2019-02-17 08:00:22 +0100

    first attempt at new skill

---
+++ skill-kindergarten/README.md
@@ -0,0 +1,13 @@
+## KINDERGARTEN
+Skill for Mycroft that communicates with kids
+
+## Description 
+This skill is meant to be used by Kids, with simple interactions and maybe, one day, the possibility to play music or "read" an audiobook
+
+
+## Examples 
+* "Hello, I am Kevin"
+
+## Credits 
+Angel Alonso Fonseca
+
---
+++ skill-kindergarten/__init__.py
@@ -0,0 +1,31 @@
+# TODO: Add an appropriate license to your skill before publishing.  See
+# the LICENSE file for more information.
+
+# Below is the list of outside modules you'll be using in your skill.
+# They might be built-in to Python, from mycroft-core or from external
+# libraries.  If you use an external library, be sure to include it
+# in the requirements.txt file so the library is installed properly
+# when the skill gets installed later by a user.
+
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill, intent_handler
+from mycroft.util.log import LOG
+
+# Each skill is contained within its own class, which inherits base methods
+# from the MycroftSkill class.  You extend this class as shown below.
+
+# TODO: Change "Template" to a unique name for your skill
+class KindergartenSkill(MycroftSkill):
+
+    # The constructor of the skill, which calls MycroftSkill's constructor
+    def __init__(self):
+        super(TemplateSkill, self).__init__(name="KindergartenSkill")
+        
+        self.count = 0
+
+    @intent_handler(IntentBuilder("").require("MyName"))
+    def handle_hello_world_intent(self, message):
+        self.speak_dialog("hello.world")
+
+def create_skill():
+    return TemplateSkill()
